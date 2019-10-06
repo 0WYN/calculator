01df2624edb8d9f126c52369f89285698551c923
0WYN <adamhalfpenny@gmail.com>
2019-10-06 12:39:41 -0400

    renamed some things and started adding data catches

--- calc.js
+++ calc.js
@@ -1,19 +1,26 @@
-const inputs = [];
+const displayText = [];
+const op = {
+  erand:[],
+  erator:[]
+}
 
-
+//capturing all click events 
 document.body.addEventListener("click", event => {
   if (event.target.nodeName == "BUTTON") {
       console.log("Clicked", event.target.id);
       if (event.target.id == "clear"){
-        while (inputs.length > 0){
-          inputs.pop();
+        while (displayText.length > 0){
+          displayText.pop();
         }
         document.getElementById("output").textContent = "";
       }
       else if (event.target.id == 'equals') {
-        compute(inputs);
+        compute(displayText);
       }
-      else {interaction(event.target.textContent);}
+      else if (event.target.id == "divide" || event.target.id == "multiply" || event.target.id == "subtract" || event.target.id == "addition") {
+        compute(event.target.id)
+      }
+      else {display(event.target.textContent);}
   }
   else{
     return
@@ -37,18 +44,24 @@ function operate(op,x,y){
 }
 
 
-function interaction(click){
-  inputs.push(click);
+function display(click){
+  displayText.push(click);
   document.getElementById("output").textContent = ""
-  const outputs = inputs.forEach(input => document.getElementById("output").textContent += input)
-  console.log(inputs);
+  const outputs = displayText.forEach(input => document.getElementById("output").textContent += input)
+  console.log(displayText);
 }
 
-function compute(){
-  const numOnlyFilter = /[^.0-9]/g;
-  let numOnly = document.getElementById('output').textContent;
-  numOnly = numOnly.split(numOnlyFilter);
+function compute(button){
+  //const numOnlyFilter = /[^.0-9]/g;
+  //let numOnly = document.getElementById('output').textContent;
+  //numOnly = numOnly.split(numOnlyFilter);
+  //console.log(numOnly);
+  console.log("Yup")
+  
+  op.erator[0] = button;
+  op.erand[0] = document.getElementById("output").textContent;
 
-  console.log(numOnly);
+  console.log(op)
+
 
 }
